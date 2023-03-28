0a24c37577131d12ef388beb62e1c5c17082ae9b
Wesley-M <wesleymatteus99@gmail.com>
2023-03-28 11:36:39 -0300

    Refactoring and making findRandomPoem to return random id

--- server/routes/poems.mjs
+++ server/routes/poems.mjs
@@ -15,11 +15,59 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Get a random poem
+// Get a random poem id
 router.get('/lucky', async (req, res) => {
     try {
-        const [ randomPoem ] = await Poem.aggregate([{ $sample: { size: 1 } }]);
-        res.status(200).json(randomPoem);
+        const [randomPoemId] = await Poem.aggregate([{ $sample: { size: 1 } }, { $project: { _id: 1 } }]);
+        res.status(200).json(randomPoemId);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+/**
+ * Get poems by search query and initial (paginated)
+ * 
+ * @param offset: pagination offset
+ * @param limit: pagination limit
+ * @param q: query string
+ * @param i: initial
+ * 
+ * @example: /poems?offset=0&limit=10
+ * @example: /poems?q=love
+ * @example: /poems?i=a
+ * @example: /poems?offset=0&limit=10&q=love&i=a
+ * 
+ * @returns {Object} Poems
+*/
+router.get('/search', async (req, res) => {
+    const offset = req.query.offset || 0;
+    const limit = req.query.limit || 10;
+
+    const query = req.query.q || "";
+    const initial = req.query.i || "";
+
+    // Filter by initial
+    const initialFilter = {
+        title: { $regex: new RegExp(`^${initial}`), $options: "i" }
+    };
+
+    // Filter by query (poet or poem title)
+    const queryFilter = {
+        $or: [
+            { title: { $regex: new RegExp(query), $options: "i" } },
+            { author: { $regex: new RegExp(query), $options: "i" } }
+        ]
+    };
+
+    // Combine filters
+    const filterBy = {
+        $and: [initialFilter, queryFilter]
+    };
+
+    try {
+        const poems = await Poem.paginate(filterBy, { offset, limit })
+        res.status(200).json(poems);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
@@ -35,60 +83,5 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// Get all poems (paginated)
-router.get('/', async (req, res) => {
-    const offset = req.query.offset || 0;
-    const limit = req.query.limit || 10;
-
-    try {
-        const poems = await Poem.paginate({}, { offset, limit })
-        res.status(200).json(poems);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-// Get poems by poet or poem title
-router.get('/search', async (req, res) => {
-    const offset = req.query.offset || 0;
-    const limit = req.query.limit || 10;
-
-    const query = req.query.query || "";
-    const filterBy = {
-        $or: [
-            {
-                title: { $regex: new RegExp(query), $options: "i" }
-            },
-            {
-                author: { $regex: new RegExp(query), $options: "i" }
-            }
-        ]
-    };
-
-    try {
-        const poems = await Poem.paginate(filterBy, { offset, limit })
-        res.status(200).json(poems);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-// Get poems by poem initial
-router.get('/initial', async (req, res) => {
-    const offset = req.query.offset || 0;
-    const limit = req.query.limit || 10;
-
-    const letter = req.query.letter || "A";
-    const filterBy = {
-        title: { $regex: new RegExp(`^${letter}`), $options: "i" }
-    };
-
-    try {
-        const poems = await Poem.paginate(filterBy, { offset, limit })
-        res.status(200).json(poems);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
 
 export default router;
